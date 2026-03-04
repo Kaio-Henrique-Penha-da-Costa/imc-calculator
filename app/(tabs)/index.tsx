2302a3b878baf9066b47283f18ddbe83835aaad1
Kaio Henrique <kaiohenriquepenhadacosta@gmail.com>
2026-03-04 15:26:15 -0300

    sesisla

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -56,7 +56,7 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#4d9af2',
+    backgroundColor: '#074c9a',
     alignItems: 'center',
     justifyContent: 'center',
   },
